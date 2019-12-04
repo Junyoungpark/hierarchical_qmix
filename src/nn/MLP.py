5d64d2e19505cbbbe87c3f72ca36a96990c17ef6
junyoung.park <junyoungpark@kaist.ac.kr>
2019-12-04 14:16:43 +0900

    add MLP

--- src/nn/MLP.py
+++ src/nn/MLP.py
@@ -111,12 +111,14 @@ class MultiLayerPerceptron(nn.Module):
                  out_activation,
                  normalization=None,
                  weight_init='xavier',
-                 dropout_probability=0.0):
+                 dropout_probability=0.0,
+                 use_noisy=False):
         super(MultiLayerPerceptron, self).__init__()
 
         self.input_dim = input_dimension
         self.output_dim = output_dimension
         self.num_neurons = num_neurons
+        self.use_noisy = use_noisy
 
         _list_norm = self.check_input_spec(normalization)
         _input_norm = True if _list_norm and len(normalization) == 1 else False
@@ -134,12 +136,12 @@ class MultiLayerPerceptron(nn.Module):
             act = activation[i] if _list_act else activation
             drop_p = dropout_probability[i] if _list_drop_p else dropout_probability
 
-            linear_module = LinearModule(in_features=in_dim, out_features=out_dim,
-                                         activation=act, norm=norm, dropout_p=drop_p, weight_init=weight_init)
+            linear_module = LinearModule(in_features=in_dim, out_features=out_dim, activation=act,
+                                         norm=norm, dropout_p=drop_p, weight_init=weight_init, use_noisy=use_noisy)
             self.layers.append(linear_module)
 
-        output_layer = LinearModule(in_features=input_dims[-1], out_features=output_dims[-1],
-                                    activation=out_activation, norm=None, dropout_p=0.0, weight_init=weight_init)
+        output_layer = LinearModule(in_features=input_dims[-1], out_features=output_dims[-1], activation=out_activation,
+                                    norm=None, dropout_p=0.0, weight_init=weight_init, use_noisy=use_noisy)
         self.layers.append(output_layer)
 
     def forward(self, x):
